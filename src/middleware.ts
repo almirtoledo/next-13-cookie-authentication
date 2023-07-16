e46856ef2e6f7371aaf1658168b017725598fd6d
Almir Toledo <deendsstudios@gmail.com>
2023-07-16 16:16:40 -0300

    feat: add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  if (!req.cookies.get("session")) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+}
+
+export const config = {
+  matcher: "/dashbaord",
+};
