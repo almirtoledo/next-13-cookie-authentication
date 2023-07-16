3c1c9f1b39902230a4cce30e5f425b9b7f6849d2
Almir Toledo <deendsstudios@gmail.com>
2023-07-16 15:42:14 -0300

    feat: add Navbar component

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export function Navbar() {
+  return (
+    <nav className="w-full h-20 bg-zinc-700 flex items-center justify-center rounded-xl shadow-lg shadow-zinc-900">
+      <span className="text-2xl text-white">
+        TASK<strong className="text-yellow-200">ME</strong>
+      </span>
+    </nav>
+  );
+}
