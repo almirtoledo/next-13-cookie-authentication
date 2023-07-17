67647c1508336295fcf40ff9a4b9763aa16d6876
Almir Toledo <deendsstudios@gmail.com>
2023-07-17 10:00:02 -0300

    refactor: alter cookies get for has

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
-  if (!req.cookies.get("session")) {
+  if (!req.cookies.has("session")) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 }
