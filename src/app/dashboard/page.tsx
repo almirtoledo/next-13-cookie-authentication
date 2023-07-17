51976b7098e38fcfee4b4f5107472b0828aa3779
Almir Toledo <deendsstudios@gmail.com>
2023-07-17 09:21:10 -0300

    feat: add get credentials and use action

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,38 @@
 "use client";
 
+import { credentials } from "@/actions/user-actions";
+import { useEffect, useState } from "react";
+
 export default function Dashbaord() {
+  const [userName, setUserName] = useState("");
+  const [userId, setUserId] = useState("");
+
+  useEffect(() => {
+    const getCredentials = async () => {
+      const data = await credentials();
+      setUserId(data.userId);
+      setUserName(data.userName);
+    };
+    getCredentials();
+  }, []);
+
+  async function handleLogout() {}
+
   return (
     <div className="space-y-6">
       <div className="flex justify-between items-end">
         <h1 className="text-2xl text-white">Dashboard</h1>
         <span className="text-md text-white">
-          Welcome <strong>Almir</strong>
+          Welcome <strong>{userName}</strong>
         </span>
       </div>
 
-      <button className="p-3 text-white rounded-xl bg-zinc-700">Sair</button>
+      <button
+        onClick={handleLogout}
+        className="p-3 text-white rounded-xl bg-zinc-700"
+      >
+        Sair
+      </button>
     </div>
   );
 }
